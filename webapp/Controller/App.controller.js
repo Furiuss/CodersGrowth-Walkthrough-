309e35e259919cd0d7066d23235338b47c031a10
Andre Messias <andremessias.az@gmail.com>
2023-04-27 10:23:21 -0300

    adicionado components na aplicacao para deixar independente

--- webapp/Controller/App.controller.js
+++ webapp/Controller/App.controller.js
@@ -6,48 +6,17 @@ sap.ui.define(
     // array de dependencias
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel" 
   ],
 
   // funcao de retorno que define modulo, as dependencias costumam
   // estar como parametros nessa funcao
-  function (Controller, MessageToast, JSONModel, ResourceModel) {
-
-    // usa a versao mais recente do js
+  function (Controller, MessageToast) {
     "use strict";
 
     // define uma nova classe do controlador com esse nome escrito na string
     return Controller.extend("sap.ui.demo.walkthrought.App", {
-      
-      // quando o controlador é iniciado ele chama essa funcao
-      onInit: function () {
-
-        // objeto dados
-        var oData = {
-          recipient: {
-            name: "UI5",
-          },
-        };
-
-        // criando dados JSONModel com o objeto criado acima
-        var oModel = new JSONModel(oData);
-
-        // pega a view atual e seta o modelo para o modelo criado acima
-        this.getView().setModel(oModel);
-
-        // seta o i18n model na view
-        var i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocale: ""
-        });
-        this.getView().setModel(i18nModel, "i18n")
-      },
-
       // funcao onShowHello
       onShowHello: function () {
-
         // ler mensagem do i18n model        
         var oBundle = this.getView().getModel("i18n").getResourceBundle();
         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
--- webapp/index.js
+++ webapp/index.js
@@ -1,14 +1,15 @@
 // crição da minha view
 
 sap.ui.define([
-  "sap/ui/core/mvc/XMLView"
-
-], function (XMLView) {
+  "sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
   "use strict";
 
-  XMLView.create({
-    viewName: "sap.ui.demo.walkthrough.view.App"
-  }).then(function (oView) {
-    oView.placeAt("content"); // depois de criada, bota essa view no meu content
-  });
+  new ComponentContainer({
+    name: "sap.ui.demo.walkthrough",
+    settings: {
+      id: "walkthrough"
+    },
+    async: true
+  }).placeAt("content")
 });
