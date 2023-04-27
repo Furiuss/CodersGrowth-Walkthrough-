6b15edf89986f26776f5e59a9d01384fa45273d3
André <andremessias.az@gmail.com>
2023-04-26 21:24:43 -0300

    json model com paradinhas a mais rsrs

--- webapp/Controller/App.controller.js
+++ webapp/Controller/App.controller.js
@@ -1,11 +1,26 @@
-sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
-], function (Controller, MessageToast) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.walkthrought.App", {
-    onShowHello: function () {
-      MessageToast.show("Hello there 😁😁")
-    }
-  })
-})
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrought.App", {
+      onInit: function () {
+        // set the data model on the view
+        var oData = {
+          recipient: {
+            name: "UI5",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+      },
+      onShowHello: function () {
+        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        MessageToast.show(`Hello ${sRecipient} 😁😁`);
+      },
+    });
+  }
+);
