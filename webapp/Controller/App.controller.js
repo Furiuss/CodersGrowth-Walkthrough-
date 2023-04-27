7f9c6deaf83cef86bfa571d51b2270ae56f52347
André <andremessias.az@gmail.com>
2023-04-26 22:37:03 -0300

    adicionando comentarios em cada linha de codigo

--- webapp/Controller/App.controller.js
+++ webapp/Controller/App.controller.js
@@ -1,24 +1,40 @@
+// definir modulo de dependencias
 sap.ui.define(
   [
+    // array de dependencias
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
   ],
+  // funcao de retorno que define modulo, as dependencias costumam
+  // estar como parametros nessa funcao
   function (Controller, MessageToast, JSONModel) {
+    // usa a versao mais recente do js
     "use strict";
+    // define uma nova classe do controlador com esse nome escrito na string
     return Controller.extend("sap.ui.demo.walkthrought.App", {
+      // quando o controlador é iniciado ele chama essa funcao
       onInit: function () {
-        // set the data model on the view
+        // objeto dados
         var oData = {
           recipient: {
             name: "UI5",
           },
         };
+        // criando dados JSONModel com o objeto criado acima
         var oModel = new JSONModel(oData);
+        // pega a view atual e seta o modelo para o modelo criado acima
         this.getView().setModel(oModel);
       },
+      // funcao onShowHello
       onShowHello: function () {
-        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        // pega a view do controller atual
+        var sRecipient = this.getView()
+        // pega o modelo da view
+          .getModel()
+        // pega o valor da propriedade name do objeto criado anteriormente
+          .getProperty("/recipient/name");
+        // chama a dependencia messageToast e da um show na variavel sRecipient
         MessageToast.show(`Hello ${sRecipient} 😁😁`);
       },
     });
