898500d8fcfbf719ad2ecaa2710bbae5d2e8889e
Andre Messias <andremessias.az@gmail.com>
2023-04-28 08:32:42 -0300

    adicionado nested views, uma separacao das views para deixar o app mais organizado

--- webapp/Controller/App.controller.js
+++ webapp/Controller/App.controller.js
@@ -4,25 +4,16 @@
 sap.ui.define(
   [
     // array de dependencias
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
+    "sap/ui/core/mvc/Controller"
   ],
 
   // funcao de retorno que define modulo, as dependencias costumam
   // estar como parametros nessa funcao
-  function (Controller, MessageToast) {
+  function (Controller) {
     "use strict";
 
     // define uma nova classe do controlador com esse nome escrito na string
-    return Controller.extend("sap.ui.demo.walkthrought.App", {
-      // funcao onShowHello
-      onShowHello: function () {
-        // ler mensagem do i18n model    
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sRecipient = this.getView().getModel().getProperty("/recipient/lastName");
-        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-        MessageToast.show(sMsg);
-      },
+    return Controller.extend("sap.ui.demo.walkthrought.App", {      
     });
   }
 );
