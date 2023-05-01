30646935a638884097250062838b61c415cd2eee
André <andremessias.az@gmail.com>
2023-05-01 09:07:23 -0300

    adicionado rotas na aplicacao

--- webapp/component.js
+++ webapp/component.js
@@ -1,31 +1,38 @@
-sap.ui.define([
-  "sap/ui/core/UIComponent",
-  "sap/ui/model/json/JSONModel",
-  "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
-  "use strict";
-  return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-     metadata : {
-         interfaces: ["sap.ui.core.IAsyncContentCreation"],
-         manifest: "json"
-     },
-     init : function () {
+sap.ui.define(
+  [
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+  ],
+  function (UIComponent, JSONModel, ResourceModel) {
+    "use strict";
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+      metadata: {
+        interfaces: ["sap.ui.core.IAsyncContentCreation"],
+        manifest: "json",
+      },
+      init: function () {
         // call the init function of the parent
         UIComponent.prototype.init.apply(this, arguments);
         // set data model
         var oData = {
-           recipient : {
-              name : "André"
-           }
+          recipient: {
+            name: "André",
+            lastName: "Messias",
+          },
         };
         var oModel = new JSONModel(oData);
         this.setModel(oModel);
 
         // set i18n model
         var i18nModel = new ResourceModel({
-           bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
         });
         this.setModel(i18nModel, "i18n");
-     }
-  });
-});
+
+        // create the views based on the url/hash
+        this.getRouter().initialize();
+      },
+    });
+  }
+);
